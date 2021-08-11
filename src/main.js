dd61d0915bf99758831872575234bca65082e359
sergdeveloper <serg.developer@yandex.ru>
2021-08-12 00:52:08 +0300

    Добавляет тестовые данные и подключает их

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import { createMoviesListTemplate } from './view/movies-list.js';
 import { createShowMoreButtonTemplate } from './view/show-more-button.js';
 import { createMovieCardTemplate } from './view/movie-card.js';
 import { createFooterStatsTemplate } from './view/footer-stats.js';
+import { generateMovie } from './mock/movie.js';
+import { createPopupMovieTemplate } from './view/popup.js';
 
-const CARDS_COUNT = 5;
+const CARDS_COUNT = 25;
+const CARDS_PER_STEP = 5;
 const CARDS_COUNT_SPECIAL = 2;
+const movies = new Array(CARDS_COUNT).fill().map(generateMovie);
+
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
@@ -15,22 +20,48 @@ const siteMainElement = document.querySelector('.main');
 const siteFooterElement = document.querySelector ('.footer');
 const siteFooterStatistics = siteFooterElement.querySelector ('.footer__statistics');
 
+const alreadyWatchedCount = movies.filter((e) => {return e.user_details.already_watched === true;});
+const favoriteCount = movies.filter((e) =>  {return e.user_details.favorite === true;});
+const watchListCount = movies.filter((e) =>  {return e.user_details.watchlist === true;});
+
 render (siteHeaderElement, createUserProfileTemplate(), 'beforeend');
-render (siteMainElement, createMenuTemplate(), 'beforeend');
+render (siteMainElement, createMenuTemplate(alreadyWatchedCount,favoriteCount, watchListCount), 'beforeend');
 render (siteMainElement, createMoviesListTemplate(), 'beforeend');
 const moviesList = siteMainElement.querySelector('.films-list');
 const moviesListContainer = siteMainElement.querySelector('.films-list__container');
 const moviesListContainerRated = siteMainElement.querySelector('.films-list__container--rated');
 const moviesListContainerCommented = siteMainElement.querySelector('.films-list__container--commented');
 
-for (let i = 0; i < CARDS_COUNT; i++) {
-  render (moviesListContainer, createMovieCardTemplate(), 'beforeend');
+for (let i = 0; i < Math.min(movies.length, CARDS_PER_STEP); i++) {
+  render (moviesListContainer, createMovieCardTemplate(movies[i]), 'beforeend');
 }
-render (moviesList, createShowMoreButtonTemplate(), 'beforeend');
+
+render (siteMainElement, createPopupMovieTemplate(movies[0]), 'beforeend');
+
+if (movies.length > CARDS_PER_STEP) {
+  render (moviesList, createShowMoreButtonTemplate(), 'beforeend');
+  let renderedMoviesCount = CARDS_PER_STEP;
+  const showMoreButton = document.querySelector('.films-list__show-more');
+  showMoreButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    movies
+      .slice(renderedMoviesCount, renderedMoviesCount + CARDS_PER_STEP)
+      .forEach((singleMovie) => render(moviesListContainer, createMovieCardTemplate(singleMovie), 'beforeend'));
+
+    renderedMoviesCount += CARDS_PER_STEP;
+
+    if (renderedMoviesCount >= movies.length) {
+      showMoreButton.remove();
+    }
+  });
+}
+
 for (let i = 0; i < CARDS_COUNT_SPECIAL; i++) {
-  render (moviesListContainerRated, createMovieCardTemplate(), 'beforeend');
+  render (moviesListContainerRated, createMovieCardTemplate(movies[i]), 'beforeend');
 }
 for (let i = 0; i < CARDS_COUNT_SPECIAL; i++) {
-  render (moviesListContainerCommented, createMovieCardTemplate(), 'beforeend');
+  render (moviesListContainerCommented, createMovieCardTemplate(movies[i]), 'beforeend');
 }
-render (siteFooterStatistics, createFooterStatsTemplate(), 'beforeend');
+render (siteFooterStatistics, createFooterStatsTemplate(movies.length), 'beforeend');
+
+export{render, movies};
--- src/view/popup.js
+++ src/view/popup.js
@@ -1,5 +1,57 @@
-export const createPopupMovieTemplate = () => (
-  `<section class="film-details">
+import dayjs from 'dayjs';
+import { render } from '../main.js';
+import { generateComment } from '../mock/comment.js';
+
+export const createUserCommentTemplate = (singleComment) => {
+  const {author, comment, date, emotion} = singleComment;
+  return`<li class="film-details__comment">
+    <span class="film-details__comment-emoji">
+      <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
+    </span>
+    <div>
+      <p class="film-details__comment-text">${comment}</p>
+      <p class="film-details__comment-info">
+        <span class="film-details__comment-author">${author}</span>
+        <span class="film-details__comment-day">${dayjs(date).format('YYYY/MM/DD HH:mm')}</span>
+        <button class="film-details__comment-delete">Delete</button>
+      </p>
+    </div>
+  </li>`;
+};
+
+export const createPopupMovieTemplate = (singleMovie) => {
+  function getTimeFromMins(time) {
+    const hours = Math.trunc(time/60);
+    const minutes = time % 60;
+    return hours + 'h ' + minutes + 'm';
+  }
+
+  const { filmInfo: { title, description, poster, total_rating, genre, director, writers, runtime, actors, release:{date, release_country}},comments, user_details:{watchlist, already_watched, favorite} } = singleMovie;
+  const commentaries = new Array(comments.length).fill().map(generateComment);
+  const createAllComments = (number) => {
+    setTimeout(() => {
+      const commentList = document.querySelector('.film-details__comments-list');
+      for (let i = 0; i<number; i++){
+        render (commentList, createUserCommentTemplate(commentaries[i]), 'beforeend');
+      }
+    }, 50);
+  };
+
+  createAllComments(commentaries.length);
+
+  const watchClassName = watchlist
+    ? 'film-details__control-button--active'
+    : '';
+
+  const alreadyWatchedClassName = already_watched
+    ? 'film-details__control-button--active'
+    : '';
+
+  const favoriteClassName = favorite
+    ? 'film-details__control-button--active'
+    : '';
+
+  return `<section class="film-details">
   <form class="film-details__inner" action="" method="get">
     <div class="film-details__top-container">
       <div class="film-details__close">
@@ -7,7 +59,7 @@ export const createPopupMovieTemplate = () => (
       </div>
       <div class="film-details__info-wrap">
         <div class="film-details__poster">
-          <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+          <img class="film-details__poster-img" src=${poster} alt="">
 
           <p class="film-details__age">18+</p>
         </div>
@@ -15,65 +67,63 @@ export const createPopupMovieTemplate = () => (
         <div class="film-details__info">
           <div class="film-details__info-head">
             <div class="film-details__title-wrap">
-              <h3 class="film-details__title">The Great Flamarion</h3>
-              <p class="film-details__title-original">Original: The Great Flamarion</p>
+              <h3 class="film-details__title">${title}</h3>
+              <p class="film-details__title-original">${title}</p>
             </div>
 
             <div class="film-details__rating">
-              <p class="film-details__total-rating">8.9</p>
+              <p class="film-details__total-rating">${total_rating}</p>
             </div>
           </div>
 
           <table class="film-details__table">
             <tr class="film-details__row">
               <td class="film-details__term">Director</td>
-              <td class="film-details__cell">Anthony Mann</td>
+              <td class="film-details__cell">${director}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Writers</td>
-              <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
+              <td class="film-details__cell">${writers}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Actors</td>
-              <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
+              <td class="film-details__cell">${actors}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Release Date</td>
-              <td class="film-details__cell">30 March 1945</td>
+              <td class="film-details__cell">${dayjs(date).format('DD MMM YYYY')}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Runtime</td>
-              <td class="film-details__cell">1h 18m</td>
+              <td class="film-details__cell">${getTimeFromMins(runtime)}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Country</td>
-              <td class="film-details__cell">USA</td>
+              <td class="film-details__cell">${release_country}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Genres</td>
               <td class="film-details__cell">
-                <span class="film-details__genre">Drama</span>
-                <span class="film-details__genre">Film-Noir</span>
-                <span class="film-details__genre">Mystery</span></td>
+                <span class="film-details__genre">${genre}</span>
             </tr>
           </table>
 
           <p class="film-details__film-description">
-            The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
+           ${description}
           </p>
         </div>
       </div>
 
       <section class="film-details__controls">
-        <button type="button" class="film-details__control-button film-details__control-button--watchlist" id="watchlist" name="watchlist">Add to watchlist</button>
-        <button type="button" class="film-details__control-button film-details__control-button--active film-details__control-button--watched" id="watched" name="watched">Already watched</button>
-        <button type="button" class="film-details__control-button film-details__control-button--favorite" id="favorite" name="favorite">Add to favorites</button>
+        <button type="button" class="film-details__control-button film-details__control-button--watchlist ${watchClassName}" id="watchlist" name="watchlist">Add to watchlist</button>
+        <button type="button" class="film-details__control-button film-details__control-button--watched ${alreadyWatchedClassName}"  id="watched" name="watched">Already watched</button>
+        <button type="button" class="film-details__control-button film-details__control-button--favorite ${favoriteClassName}" id="favorite" name="favorite">Add to favorites</button>
       </section>
     </div>
 
     <div class="film-details__bottom-container">
       <section class="film-details__comments-wrap">
-        <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
+        <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
 
         <ul class="film-details__comments-list">
 
@@ -111,5 +161,5 @@ export const createPopupMovieTemplate = () => (
       </section>
     </div>
   </form>
-</section>`
-);
+</section>`;
+};
